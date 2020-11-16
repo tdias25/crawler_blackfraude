cdf2b3ca28c091cc8baa3be1f9b299490c3ea076
Thiago Dias <thiago_dll@hotmail.com>
2020-11-16 02:06:23 -0300

    fixed async problem inside forEach loop

--- src/index.js
+++ src/index.js
@@ -11,24 +11,29 @@ const Terabyte = require('./crawlers/terabyte')
 
 let crawlers = [
     Kabum,
-    Terabyte
-]
+    // Terabyte
+];
+
+(async function () {
+
+    for (crawler of crawlers) {
+
+        for (productData of products) {
+
+            let expectedProduct = JsonProductFactory(productData)
+            let currentProduct = await crawler.findProduct(expectedProduct.getName())
 
 
-crawlers.forEach((crawler) => {
+            if (currentProduct.getPrice() <= expectedProduct.getPrice()) {
+                console.log(` "${expectedProduct.getName()}" está gostosin no azeite com o valor de ${expectedProduct.getPrice()}`)
+                //     telegramProductNotifier.notify(product)
+            } else {
+                console.log(` "${expectedProduct.getName()}" não está com preço legal :(`)
+            }
 
-    products.forEach(async (productData) => {
-
-        let expectedProduct = JsonProductFactory(productData)
-        let currentProduct = await crawler.findProduct(expectedProduct.getName())
-
-        if (currentProduct.getPrice() <= expectedProduct.getPrice()) {
-            console.log(`${expectedProduct.getName()} esta em promocao`)
-            //     telegramProductNotifier.notify(product)
-        } else {
-            console.log(`${expectedProduct.getName()} nao esta batendo certo`)
         }
+    }
 
-    })
+})()
 
-})
+
